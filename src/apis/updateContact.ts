bba00dd5cb57a672299dd0ea639600c48f5c81c4
Alpacorp <alejandro.palacios88@gmail.com>
2023-02-10 20:36:17 -0500

    Create api call to update contact data

---
+++ src/apis/updateContact.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+let baseURL = "";
+
+if (import.meta.env.VITE_MODE === "production") {
+  baseURL = import.meta.env.VITE_API_URL_PROD;
+} else if (import.meta.env.VITE_MODE === "development") {
+  baseURL = import.meta.env.VITE_API_URL_DEV;
+}
+
+export const apiUpdateContact = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
